1f6999df53bf1f8bc7a67e931739afcd654659fa
Zoey Baker <zoeybaker625@gmail.com>
2023-01-03 17:19:24 -0600

    initial commit

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,13 +4,15 @@ namespace StaticExercise
 {
     class Program
     {
+        private static object celcis;
+
         static void Main(string[] args)
         {
             var celsius = TempConverter.FahrenheitToCelsius(68); // 20
             var fahrenheit = TempConverter.CelsiusToFahrenheit(20); //68
 
             Console.WriteLine("After conversion:");
-            Console.WriteLine($"celcius {celcius}");
+            Console.WriteLine($"celcis {celcis}");
             Console.WriteLine($"fahrenheit {fahrenheit}");
         }
     }
